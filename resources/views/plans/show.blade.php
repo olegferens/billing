bbbb984a4040c0576d2a0aab96ccf0354a0b2357
Oleg Ferens <oleg.feens@gmail.com>
2018-09-02 13:51:16 +0300

    Braintree Token Generate in Ajax call

--- resources/views/plans/show.blade.php
+++ resources/views/plans/show.blade.php
@@ -7,12 +7,14 @@
         <div class="panel panel-default">
           <div class="panel-heading">{{ $plan->name }}</div>
           <div class="panel-body">
-             <form>
-              <div id="dropin-container"></div>
-              <hr>
 
-              <button id="payment-button" class="btn btn-primary btn-flat hidden" type="submit">Pay now</button>
-            </form>
+<form action="{{ url('/subscribe') }}" method="post">
+  <div id="dropin-container"></div>
+  <input type="hidden" name="plan" value="{{ $plan->id }}">
+  {{ csrf_field() }}
+  <hr>
+  <button id="payment-button" class="btn btn-primary btn-flat hidden" type="submit">Pay now</button>
+</form>
 
           </div>
         </div>
@@ -25,8 +27,15 @@
     <script src="https://js.braintreegateway.com/js/braintree-2.30.0.min.js"></script>
 
     <script>
-     braintree.setup('CLIENT-TOKEN-FROM-SERVER', 'dropin', {
-       container: 'dropin-container'
-     });
+       $.ajax({
+            url: '{{ url('braintree/token') }}'
+        }).done(function (response) {
+            braintree.setup(response.data.token, 'dropin', {
+                container: 'dropin-container',
+                onReady: function () {
+                    $('#payment-button').removeClass('hidden');
+                }
+            });
+        });
     </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/plans', 'PlansController@index');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/plan/{plan}', 'PlansController@show');
+    Route::get('/braintree/token', 'BraintreeTokenController@token');
 });
