1030eda908d6539c209bb9bce1860a20a3f3d7a9
Oleg Ferens <oleg.feens@gmail.com>
2018-09-02 13:14:17 +0300

    Plan controller

---
+++ app/Http/Controllers/PlansController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Plan;
+
+class PlansController extends Controller
+{
+    public function index()
+    {
+        return view('plans.index')->with(['plans' => Plan::get()]);
+    }
+}
