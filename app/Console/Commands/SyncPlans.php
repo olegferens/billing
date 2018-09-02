959de66713e505e05258680769c5bc3918f3924d
Oleg Ferens <oleg.feens@gmail.com>
2018-09-02 13:08:32 +0300

    Sync Plans command implementation

--- app/Console/Commands/SyncPlans.php
+++ app/Console/Commands/SyncPlans.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Braintree_Plan;
+use App\Plan;
 
 class SyncPlans extends Command
 {
@@ -37,6 +39,24 @@ class SyncPlans extends Command
      */
     public function handle()
     {
-        //
+        // Empty table
+        Plan::truncate();
+
+        // Get plans from Braintree
+        $braintreePlans = Braintree_Plan::all();
+
+        // uncomment the line below to dump the plans when running the command
+        // var_dump($braintreePlans);
+
+        // Iterate through the plans while populating our table with the plan data
+        foreach ($braintreePlans as $braintreePlan) {
+            Plan::create([
+                'name' => $braintreePlan->name,
+                'slug' => str_slug($braintreePlan->name),
+                'braintree_plan' => $braintreePlan->id,
+                'cost' => $braintreePlan->price,
+                'description' => $braintreePlan->description,
+            ]);
+        }
     }
 }
