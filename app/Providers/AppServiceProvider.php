cbfeb2433c4dafcc0391515985e021e445ecb641
Oleg Ferens <oleg.feens@gmail.com>
2018-09-02 12:49:46 +0300

    Braintree Boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Braintree_Configuration;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       Schema::defaultStringLength(191); 
+        Schema::defaultStringLength(191); 
+        Braintree_Configuration::environment(env('BRAINTREE_ENV'));
+        Braintree_Configuration::merchantId(env('BRAINTREE_MERCHANT_ID'));
+        Braintree_Configuration::publicKey(env('BRAINTREE_PUBLIC_KEY'));
+        Braintree_Configuration::privateKey(env('BRAINTREE_PRIVATE_KEY'));
+        // Cashier::useCurrency('eur', '€');
     }
 
     /**
